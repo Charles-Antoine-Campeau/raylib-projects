d5f4fcf6faf1e1f7318464d0b36fd97909840655
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-08-14 12:21:45 -0400

    Create 3d Camera which focus on a grid with a sphere in the middle.

--- left-right-sphere/left-right-sphere.c
+++ left-right-sphere/left-right-sphere.c
@@ -1,9 +1,22 @@
+/*
+* First program with raylib.  Consist of moving a sphere back and forth from left to right.
+*/
+
 #include "raylib.h"
 
 int main(void)
 {
     const int screenWidth = 1200;
     const int screenHeight = 800;
+    
+    Camera camera = {0};
+    camera.position = (Vector3){10.0f, 10.0f, 10.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    
+    Vector3 spherePosition = {0.0f, 0.0f, 0.0f};
     
     InitWindow(screenWidth, screenHeight, "Left right sphere");
     
@@ -12,9 +25,14 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        
         ClearBackground(RAYWHITE);
+        BeginMode3D(camera);
         
+        DrawSphere(spherePosition, 1, RED);
+        
+        DrawGrid(10, 1.0f);
+        
+        EndMode3D();
         EndDrawing();
     }
     
