efd633b6ce914a0831abde0346d2cc0c086d49a3
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-08-14 11:30:12 -0400

    Creating window

--- left-right-sphere/left-right-sphere.c
+++ left-right-sphere/left-right-sphere.c
@@ -2,5 +2,23 @@
 
 int main(void)
 {
+    const int screenWidth = 1200;
+    const int screenHeight = 800;
     
+    InitWindow(screenWidth, screenHeight, "Left right sphere");
+    
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    
+    return 0;
 }
