98447e99e140b98a2dc7770b88ff1c15f484b3ba
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-08-17 09:22:35 -0400

    Draw a circle and moves it in a specific direction.

--- boids/v2/boids_2D.c
+++ boids/v2/boids_2D.c
@@ -3,5 +3,30 @@
 
 int main(void)
 {
+    const int screenHeight = 1000;
+    const int screenWidth = 1400;
     
+    InitWindow(screenWidth, screenHeight, "Boid 2D");
+    SetTargetFPS(60);
+    
+    
+    int CenterX[] = {20};
+    int CenterY[] = {20};
+    
+    Vector2 direction[] = {{1,1}};
+    
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        //UPDATE***************************************************
+        CenterX[0] += direction[0].x;
+        CenterY[0] += direction[0].y;
+        
+        //DRAW*****************************************************
+        DrawCircle(CenterX[0], CenterY[0], 15, RED);
+        
+        EndDrawing();
+    }
 }
