a67db52ab6d7ae93b05cdcff20696588feaa66d0
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-08-17 11:31:13 -0400

    Adding comments

--- boids/v2/boids_2D.c
+++ boids/v2/boids_2D.c
@@ -3,19 +3,24 @@
 
 int main(void)
 {
+    //constants for circles
     const int screenHeight = 1000;
     const int screenWidth = 1400;
     
+    //constants for circles
     const int numberOfCircles = 15;
     const int radius = 5;
     
-    InitWindow(screenWidth, screenHeight, "Boid 2D");
-    SetTargetFPS(60);
-    
+    //variables for circles
     int CenterX[numberOfCircles];
     int CenterY[numberOfCircles];
     Vector2 direction[numberOfCircles];
     
+    //initialise the screen
+    InitWindow(screenWidth, screenHeight, "Boid 2D");
+    SetTargetFPS(60);
+    
+    //set the starting point and starting vector for each circle
     int start = 20;
     for(int i = 0; i  < numberOfCircles; i++)
     {
@@ -35,11 +40,11 @@ int main(void)
         //UPDATE****************************************************************************
         for(int i = 0; i < numberOfCircles; i++)
         {
+            //change the direction of the circle if it has reach one of the side
             if((CenterX[i] >= (screenWidth - radius*2)) || (CenterX[i] <= radius*2))
             {
                 direction[i].x = -direction[i].x;
             }
-            
             if(CenterY[i] >= (screenHeight - radius*2) || CenterY[i] <= radius*2)
             {
                 direction[i].y = -direction[i].y;
