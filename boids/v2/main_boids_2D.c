255f62e2c1510ad0c6aa2c47c578416da4fde48a
Charles-Antoine-Campeau <campeauca@gmail.com>
2021-08-18 10:43:10 -0400

    changing position variables to Vector2

--- boids/v2/main_boids_2D.c
+++ boids/v2/main_boids_2D.c
@@ -15,8 +15,7 @@ int main(void)
                            SKYBLUE, DARKBLUE, PURPLE, DARKPURPLE, BROWN};
     
     //variables for circles
-    int CenterX[numberOfCircles];
-    int CenterY[numberOfCircles];
+    Vector2 position[numberOfCircles];
     Vector2 direction[numberOfCircles];
 
     //initialise the screen
@@ -27,8 +26,7 @@ int main(void)
     int start = 20;
     for(int i = 0; i  < numberOfCircles; i++)
     {
-        CenterX[i] = start;
-        CenterY[i] = GetRandomValue(40, 250);
+        position[i] = (Vector2){start, GetRandomValue(40, 300)};
         
         start += 60;
         
@@ -44,11 +42,11 @@ int main(void)
         for(int i = 0; i < numberOfCircles; i++)
         {
             //change the direction of the circle if it has reach one of the side
-            if((CenterX[i] >= (screenWidth - radius*2)) || (CenterX[i] <= radius*2))
+            if((position[i].x >= (screenWidth - radius*2)) || (position[i].x <= radius*2))
             {
                 direction[i].x = -direction[i].x;
             }
-            if(CenterY[i] >= (screenHeight - radius*2) || CenterY[i] <= radius*2)
+            if(position[i].y >= (screenHeight - radius*2) || position[i].y <= radius*2)
             {
                 direction[i].y = -direction[i].y;
             }
@@ -58,21 +56,21 @@ int main(void)
             //Change the direction of the circle if another circle is going to collide with it
             for(int j = i; j < numberOfCircles; j++)
             {
-                centerOfGravityX += CenterX[j];
-                centerOfGravityY += CenterY[j];
+                centerOfGravityX += position[j].x;
+                centerOfGravityY += position[j].y;
                 //make sure that the compared circle is not the same
                 if(i != j)
                 {
-                    int deltaX = CenterX[j] - CenterX[i];
-                    int deltaY = CenterY[j] - CenterY[j];
+                    int deltaX = position[j].x - position[i].x;
+                    int deltaY = position[j].y - position[j].y;
                     
                     //verify the distance between the 2 circles
                     if (deltaX <= radius*5 || deltaY <= radius*5)
                     {
                         //determine the type of tragectory
-                        if(CenterX[i] == CenterX[j])
+                        if(position[i].x == position[j].x)
                         {
-                            if(CenterY[i] == CenterY[j])
+                            if(position[i].y == position[j].y)
                             {
                                 //do nothing, parrallel
                             }
@@ -83,7 +81,7 @@ int main(void)
                         }
                         else
                         {
-                            if(CenterY[i] == CenterY[j])
+                            if(position[i].y == position[j].y)
                             {
                                 direction[i].x = -direction[i].x;
                             }
@@ -101,8 +99,8 @@ int main(void)
             }
 
             //move the circle
-            CenterX[i] += direction[i].x;
-            CenterY[i] += direction[i].y;
+            position[i].x += direction[i].x;
+            position[i].y += direction[i].y;
             
             
         }
@@ -110,7 +108,7 @@ int main(void)
         //DRAW******************************************************************************
         for(int i = 0; i < numberOfCircles; i++)
         {
-            DrawCircle(CenterX[i], CenterY[i], radius, color[i]);
+            DrawCircle(position[i].x, position[i].y, radius, color[i]);
         }
         
         
